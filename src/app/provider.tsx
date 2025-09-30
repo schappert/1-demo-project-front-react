3b444f148d40f829aedcdc33b68f1e607a50983a
sebastien_chapert <sebastien.chappert@hotmail.fr>
2025-09-30 10:15:03 +0200

    Resolve imports

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,7 +3,7 @@ import { store } from "./store";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./queryClient";
 import { BrowserRouter } from "react-router-dom";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export function AppProviders({ children }: { children: ReactNode }) {
     return (
