c9af994f41dda39bbea605d1a723b73b9f38ecd5
sebastien_chapert <sebastien.chappert@hotmail.fr>
2025-09-30 10:44:27 +0200

    Configurations Axios

--- src/api/client.ts
+++ src/api/client.ts
@@ -8,7 +8,7 @@ export const api = axios.create({
 });
 
 export const login = async (username: string, password: string) => {
-    const res = await axios.post(`${baseURL}/auth/login`, { username, password });
+    const res = await axios.post(`/auth/login`, { username, password });
     // backend renvoie { access_token: "xxxx" }
     return res.data;
 };
--- src/features/dashboard/dashboard-page.tsx
+++ src/features/dashboard/dashboard-page.tsx
@@ -13,7 +13,7 @@ export const DashboardPage = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            const res = await axios.get("https://sebastien-chappert.fr/dashboard", {
+            const res = await axios.get("/dashboard", {
                 headers: { Authorization: `Bearer ${token}` },
             });
             setData(res.data);
